da63f102d2b605cf8ffdd22c10cf14bcd0bd821d
abboutah <abboutah@student.1337.ma>
2024-01-17 15:57:36 +0100

    up

--- 04-python/09-exceptions.py
+++ 04-python/09-exceptions.py
@@ -1,10 +1,14 @@
 import sys
-x = int(input("x: "))
-y = int(input("y: "))
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error: Invalid input.")
+    sys.exit(1)
 try:
     result = x / y
 except ZeroDivisionError:
-    print("Error: cannot divide by 0.")
+    print("Error: Cannot divide by 0.")
     sys.exit(1)
 print(f"{x} / {y} = {result}")
 
