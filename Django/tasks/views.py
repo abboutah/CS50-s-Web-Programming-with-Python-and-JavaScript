4eb576df013f8ba0fd1452340dfcafad0ff6a0d4
abboutah <abboutah@student.1337.ma>
2024-01-19 13:58:26 +0100

    comment

--- Django/tasks/views.py
+++ Django/tasks/views.py
@@ -9,23 +9,42 @@ class NewTaskForm(forms.Form):
 
 # Create your views here.
 def index(request):
+
+    # Check if there already exists a "tasks" key in our session
+
     if "tasks" not in request.session:
+
+        # If not, create a new list
         request.session["tasks"] = []
-    
+
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
     })
+
+# Add a new task:
 def add(request):
-    if request.method == "POST": 
-        form = NewTaskForm(request.POST) #figure out all data submitted
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
         if form.is_valid():
-            task = form.cleaned_data["task"] #add to task variable
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
             request.session["tasks"] += [task]
+
+            # Redirect user to list of tasks
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
-             return render(request, "tasks/add.html", {
-             "form": form
-                })
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
 
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
