1dbb30ba8e861b2b01c6928c791ff9211f3aecf2
abboutah <abboutah@student.1337.ma>
2024-01-22 16:06:09 +0100

    init sql

---
+++ SQL/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO flights (origin, destination, duration) VALUES ("Marrakech", "Madrid", 120 );
+INSERT INTO flights (origin, destination, duration) VALUES ("Amsterdam", "Moscow",  195 );
+INSERT INTO flights (origin, destination, duration) VALUES ("Dubai", "Rome", 400 );
+INSERT INTO flights (origin, destination, duration) VALUES ("Tunis", "London",  180);
+INSERT INTO flights (origin, destination, duration) VALUES ("Beirut", "Istanbul", 120 );
+INSERT INTO flights (origin, destination, duration) VALUES ("Los Angeles", "New York",  320);
+
